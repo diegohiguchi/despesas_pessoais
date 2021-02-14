2ed13c019950a510608fd02c47df36dc1f18f58d
Diego Higuchi <diegohiguchi@gmail.com>
2021-02-14 15:44:52 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:despesas_pessoais/components/transaction_user.dart';
+import 'package:flutter/material.dart';
+
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          TransactionUser()
+        ],
+      ),
+    );
+  }
+}
